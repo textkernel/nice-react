ee35a4f5b653ef2d7725bdcdf4ebfcc23c8d131c
Mauricio Minella <mauriciominella@gmail.com>
2017-11-09 22:08:05 +0100

    Renaming the style and size properties in order to avoid conflicts with reserved words

--- tests/Buttons/Button.spec.js
+++ tests/Buttons/Button.spec.js
@@ -6,4 +6,19 @@ describe('<Button />', () => {
     const wrapper = shallow(<Button>Default</Button>);
     expect(wrapper).toMatchSnapshot();
   });
+
+  it('should render style Button', () => {
+    const wrapper = shallow(<Button ncStyle="primary">Primary</Button>);
+    expect(wrapper).toMatchSnapshot();
+  });
+
+  it('should render size Button', () => {
+    const wrapper = shallow(<Button ncSize="large">Large</Button>);
+    expect(wrapper).toMatchSnapshot();
+  });
+
+  it('should render style and size Button', () => {
+    const wrapper = shallow(<Button ncStyle="primary" ncSize="large">Primary large</Button>);
+    expect(wrapper).toMatchSnapshot();
+  });
 });
