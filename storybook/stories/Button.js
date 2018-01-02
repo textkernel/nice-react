33ba8e114837950178e810108c772b7e873c0b2c
Mauricio Minella <minella@textkernel.nl>
2018-01-02 15:33:16 +0100

    Restricting button context and sizes values using prop-types

--- storybook/stories/Button.js
+++ storybook/stories/Button.js
@@ -1,19 +1,43 @@
 import React from 'react';
 import { storiesOf } from '@storybook/react';
+import { withKnobs } from '@storybook/addon-knobs';
+import { withInfo } from '@storybook/addon-info';
 import { action } from '@storybook/addon-actions';
 
 import { Button } from 'nice-react';
 
-storiesOf('Button', module)
-  .add('Default', () => (
-    <Button onClick={() => alert('Default')}>Default</Button>
-  ))
-  .add('Primary', () => (
-    <Button context="primary">Primary</Button>
-  ))
-  .add('Large', () => (
-    <Button size="large">Large</Button>
-  ))
-  .add('Small', () => (
-    <Button size="small">Small</Button>
-  ));;
+const stories = storiesOf('Button', module);
+stories.addDecorator((story, context) => withInfo('')(story)(context));
+stories.addDecorator(withKnobs);
+
+stories.add('Default', () => (
+  <Button onClick={() => alert('Default')}>Default</Button>
+));
+
+stories.add('Primary', () => (
+  <Button context="primary">Primary</Button>
+));
+
+stories.add('Warning', () => (
+  <Button context="warning">Warning</Button>
+));
+
+stories.add('Good', () => (
+  <Button context="good">Good</Button>
+));
+
+stories.add('Bad', () => (
+  <Button context="bad">Bad</Button>
+));
+
+stories.add('Large', () => (
+  <Button size="large">Large</Button>
+));
+
+stories.add('Medium', () => (
+  <Button size="medium">Medium</Button>
+));
+
+stories.add('Small', () => (
+  <Button size="small">Small</Button>
+));
