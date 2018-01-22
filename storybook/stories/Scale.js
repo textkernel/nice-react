413fd9162393f44e7854a7a1aea3a49ab01bdae0
Guilherme <ribeiro@textkernel.nl>
2018-01-22 09:46:33 +0100

    Scale component more composable

--- storybook/stories/Scale.js
+++ storybook/stories/Scale.js
@@ -3,28 +3,73 @@ import { storiesOf } from '@storybook/react';
 import { withInfo } from '@storybook/addon-info';
 import { action } from '@storybook/addon-actions';
 
-import { Scale } from 'nice-react';
+import { ScaleGroup, ScaleItem } from 'nice-react';
 
 const stories = storiesOf('Scale', module);
 
 stories.addDecorator((story, context) => withInfo('')(story)(context));
 
 stories.add('with all labels', () => {
+  
+  return (
+    <ScaleGroup name='projectid-radios' onChange={ action('radio-click') }> 
+      <ScaleItem label='Nice to have' value={1} />
+      <ScaleItem label='Good to have' value={5} />
+      <ScaleItem label='Must have' value={10} />
+    </ScaleGroup>
+  );
+});
+
+stories.add('without one label', () => {
+  
+  return (
+    <ScaleGroup name='projectid-radios' onChange={ action('radio-click') }> 
+      <ScaleItem label='Nice to have' value={1} />
+      <ScaleItem value={5} />
+      <ScaleItem label='Must have' value={10} />
+    </ScaleGroup>
+  );
+});
+
+stories.add('without tick labels', () => {
+  return (
+    <ScaleGroup name='projectid-radios' tickLabels={false} onChange={ action('radio-click') }> 
+      <ScaleItem label='Nice to have' value={1} />
+      <ScaleItem value={5} />
+      <ScaleItem label='Must have' value={10} />
+    </ScaleGroup>
+  );
+});
+  
+
+stories.add('with custom classes over items', () => {
+  const items = [
+    { label: 'Nice to have', value: 1, className: 'text-bold' },
+    { label: 'Good to have', value: 5, className: 'text-bold text-italic' },
+    { label: 'Must have', value: 10, className: 'text-uppercase text-brand' }
+  ];
+  
+  return (
+    <ScaleGroup name='projectid-radios' tickLabels={false} onChange={ action('radio-click') }> 
+      <ScaleItem label='Nice to have' value={1} className='text-bold'/>
+      <ScaleItem value={5} className='text-bold text-italic' />
+      <ScaleItem label='Must have' value={10} className='text-uppercase text-brand' />
+    </ScaleGroup>
+  );
+});
+
+stories.add('with custom classes over outermost element', () => {
   const items = [
     { label: 'Nice to have', value: 1 },
     { label: 'Good to have', value: 5 },
     { label: 'Must have', value: 10 }
   ];
   
-  return <Scale items={items} onChange={ action('radio-click') } radioGroupName='projectid-radios' />
-});
-
-stories.add('omitting some labels', () => {
-  const items = [
-    { label: 'Nice to have', value: 1 },
-    { label: 'Good to have', omitLabel: true, value: 5 },
-    { label: 'Must have', value: 10 }
-  ];
-  
-  return <Scale items={items} onChange={ action('radio-click') } radioGroupName='projectid-radios' />
+  return (
+    <ScaleGroup name='projectid-radios' className='nice-bg-info' onChange={ action('radio-click') }> 
+      <ScaleItem label='Nice to have' value={1} />
+      <ScaleItem value={5} />
+      <ScaleItem label='Must have' value={10} />
+    </ScaleGroup>
+  );
 });
