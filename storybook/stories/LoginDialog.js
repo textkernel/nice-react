4056b0b9413c3a4d9e6c7f11c596806d6134eead
Carlo Bernardini <bernardini@textkernel.nl>
2018-03-01 16:27:52 +0100

    Improved LoginDialog

--- storybook/stories/LoginDialog.js
+++ storybook/stories/LoginDialog.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import { storiesOf } from '@storybook/react';
-import { withKnobs } from '@storybook/addon-knobs';
+import { withKnobsOptions, withKnobs, boolean, select, text } from '@storybook/addon-knobs';
 import { withInfo } from '@storybook/addon-info';
 import props from '../../lib/props';
 
@@ -11,37 +11,54 @@ const stories = storiesOf('Login Dialog', module);
 stories.addDecorator((story, context) => withInfo('')(story)(context));
 stories.addDecorator(withKnobs);
 
-stories.add('Default', () => (
-    <LoginDialog
-        product="jobfeed"
-        intro="Welcome to Textkernel. Please log in using your credentials to access your account."
-        version="Nice! React v1.2.3"
-        onSubmit={ e => {
-            console.log (e);
-        }}
+stories.add('Default', () => {
+    const brand = text('Brand', 'textkernel');
+    const product = select('Product', props.products);
+    const intro = text('Intro text', 'Welcome to Textkernel. Please log in using your credentials to access your account.')
+    const version = text('Version', 'Nice! React v1.2.3');
+    const loading = boolean('Loading', false);
+    const attribution = boolean('Show attribution', true);
+
+    return (<LoginDialog
+        brand={ brand }
+        product={ product }
+        intro={ intro }
+        version={ version }
+        loading={ loading }
+        noAttribution={ !attribution }
     >
-        <div className="nice-form-group">
-            <label htmlFor="nice-login-username">Username</label>
-            <input type="email" placeholder="Username..." className="nice-form-control" id="nice-login-username" />
-        </div>
-        <div className="nice-form-group">
-            <label htmlFor="nice-login-account">Account</label>
-            <input type="text" placeholder="Account name..." className="nice-form-control" id="nice-login-account" />
-        </div>
-        <div className="nice-form-group">
-            <label htmlFor="nice-login-password">Password</label>
-            <input type="password" placeholder="Password..." className="nice-form-control" id="nice-login-password" />
-        </div>
-        <div className="nice-form-group">
-            <input type="checkbox" className="nice-toggle" id="login-remember" />
-            <label className="nice-toggle-btn off-muted" htmlFor="login-remember">
-                Remember me on this device
-            </label>
-        </div>
-        <LoginDialog.Footer>
-            <Button context="primary" submit>
-                Login
-            </Button>
-        </LoginDialog.Footer>
-    </LoginDialog>
-));
+        <form onSubmit={ e => {
+            e.preventDefault();
+        }}>
+            <div className="nice-form-group">
+                <label htmlFor="nice-login-username">Username</label>
+                <input type="email" placeholder="Username..." className="nice-form-control" id="nice-login-username" />
+            </div>
+            <div className="nice-form-group">
+                <label htmlFor="nice-login-account">Account</label>
+                <input type="text" placeholder="Account name..." className="nice-form-control" id="nice-login-account" />
+            </div>
+            <div className="nice-form-group">
+                <label htmlFor="nice-login-password">Password</label>
+                <input type="password" placeholder="Password..." className="nice-form-control" id="nice-login-password" />
+            </div>
+            <div className="nice-form-group">
+                <input type="checkbox" className="nice-toggle" id="login-remember" />
+                <label className="nice-toggle-btn off-muted" htmlFor="login-remember">
+                    Remember me on this device
+                </label>
+            </div>
+            <div className="nice-form-group">
+                <Button context="link" submit style={{
+                    paddingLeft: 0,
+                    paddingRight: 0
+                }}>
+                    <i className="icon fa-question" /> Lost your credentials?
+                </Button>
+                <Button context="primary" submit pullRight>
+                    Login
+                </Button>
+            </div>
+        </form>
+    </LoginDialog>);
+});
---
+++ tests/LoginDialog/LoginDialog.js
@@ -0,0 +1,97 @@
+import React from 'react';
+import LoginDialog from '../../lib/LoginDialog';
+import Button from '../../lib/Button';
+
+describe('<LoginDialog />', () => {
+    it('should render a default LoginDialog', () => {
+        const wrapper = shallow(<LoginDialog
+            brand="textkernel"
+            product="textkernel"
+            intro="Welcome to Textkernel. Please log in using your credentials to access your account."
+            version="Nice v1.2.3"
+        >
+            <form onSubmit={ e => {
+                e.preventDefault();
+            }}>
+                <div className="nice-form-group">
+                    <label htmlFor="nice-login-username">Username</label>
+                    <input type="email" placeholder="Username..." className="nice-form-control" id="nice-login-username" />
+                </div>
+                <div className="nice-form-group">
+                    <label htmlFor="nice-login-account">Account</label>
+                    <input type="text" placeholder="Account name..." className="nice-form-control" id="nice-login-account" />
+                </div>
+                <div className="nice-form-group">
+                    <label htmlFor="nice-login-password">Password</label>
+                    <input type="password" placeholder="Password..." className="nice-form-control" id="nice-login-password" />
+                </div>
+                <div className="nice-form-group">
+                    <input type="checkbox" className="nice-toggle" id="login-remember" />
+                    <label className="nice-toggle-btn off-muted" htmlFor="login-remember">
+                        Remember me on this device
+                    </label>
+                </div>
+                <div className="nice-form-group">
+                    <Button context="link" submit style={{
+                        paddingLeft: 0,
+                        paddingRight: 0
+                    }}>
+                        <i className="icon fa-question" /> Lost your credentials?
+                    </Button>
+                    <Button context="primary" submit className="pull-right">
+                        Login
+                    </Button>
+                </div>
+            </form>
+        </LoginDialog>);
+
+        expect(wrapper).toMatchSnapshot();
+    });
+
+    it('should render a loading LoginDialog without attribution / version', () => {
+        const wrapper = shallow(<LoginDialog
+            brand="textkernel"
+            product="textkernel"
+            intro="Welcome to Textkernel. Please log in using your credentials to access your account."
+            version="Nice v1.2.3"
+            noAttribution
+            loading
+        >
+            <form onSubmit={ e => {
+                e.preventDefault();
+            }}>
+                <div className="nice-form-group">
+                    <label htmlFor="nice-login-username">Username</label>
+                    <input type="email" placeholder="Username..." className="nice-form-control" id="nice-login-username" />
+                </div>
+                <div className="nice-form-group">
+                    <label htmlFor="nice-login-account">Account</label>
+                    <input type="text" placeholder="Account name..." className="nice-form-control" id="nice-login-account" />
+                </div>
+                <div className="nice-form-group">
+                    <label htmlFor="nice-login-password">Password</label>
+                    <input type="password" placeholder="Password..." className="nice-form-control" id="nice-login-password" />
+                </div>
+                <div className="nice-form-group">
+                    <input type="checkbox" className="nice-toggle" id="login-remember" />
+                    <label className="nice-toggle-btn off-muted" htmlFor="login-remember">
+                        Remember me on this device
+                    </label>
+                </div>
+                <div className="nice-form-group">
+                    <Button context="link" submit style={{
+                        paddingLeft: 0,
+                        paddingRight: 0
+                    }}>
+                        <i className="icon fa-question" /> Lost your credentials?
+                    </Button>
+                    <Button context="primary" submit className="pull-right">
+                        Login
+                    </Button>
+                </div>
+            </form>
+        </LoginDialog>);
+
+        expect(wrapper).toMatchSnapshot();
+    });
+});
