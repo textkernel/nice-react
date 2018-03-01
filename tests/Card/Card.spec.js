cfdf92204a84613588cf4cb0bc806b256b87b340
Carlo Bernardini <bernardini@textkernel.nl>
2018-03-01 12:47:23 +0100

    Added Card component with story and tests

---
+++ tests/Card/Card.spec.js
@@ -0,0 +1,51 @@
+import React from 'react';
+import Card from '../../lib/Card';
+
+const onClick = () => {};
+
+describe('<Card />', () => {
+    it('should render default Card without subs', () => {
+        const wrapper = shallow(<Card
+            title="This is a card"
+            controls={ [{
+                label: 'OK',
+                className: 'icon fa-check',
+                onClick
+            }, {
+                label: 'Dismiss',
+                disabled: true,
+                onClick
+            }] }
+            image="//placeholdit.imgix.net/~text?txtsize=33&txt=300%C3%97150&w=300&h=150"
+            style={{
+                float: 'none',
+                width: '300px'
+            }}
+        >
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Curabitur
+            ullamcorper nulla ac quam hendrerit pharetra.
+        </Card>);
+        expect(wrapper).toMatchSnapshot();
+    });
+
+    it('should render Card with subs, without controls', () => {
+        const wrapper = shallow(<Card
+            title="This is a card with subs"
+            context="brand"
+            style={{
+                float: 'none',
+                width: '300px'
+            }}
+        >
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Curabitur
+            ullamcorper nulla ac quam hendrerit pharetra.
+            <Card.Sub className="icon fa-reply">
+                In semper vel lectus sed finibus. Donec in erat risus. Aenean in lectus massa. Phasellus id malesuada arcu, vel ultricies enim.
+            </Card.Sub>
+            <Card.Sub className="icon fa-reply">
+                    In semper vel lectus sed finibus. Donec in erat risus. Aenean in lectus massa. Phasellus id malesuada arcu, vel ultricies enim.
+            </Card.Sub>
+        </Card>);
+        expect(wrapper).toMatchSnapshot();
+    });
+});
