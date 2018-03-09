f94ea0e429efec7c26c24745b95c4daa0a4bd8bd
Carlo Bernardini <bernardini@textkernel.nl>
2018-03-09 10:10:50 +0100

    Rebuilt dist

--- storybook/stories/Loading.js
+++ storybook/stories/Loading.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import { storiesOf } from '@storybook/react';
-import { withKnobsOptions, withKnobs, text, boolean, select } from '@storybook/addon-knobs';
+import { withKnobs, boolean, select } from '@storybook/addon-knobs';
 import { withInfo } from '@storybook/addon-info';
 import props from '../../lib/props';
 
@@ -12,20 +12,25 @@ stories.addDecorator((story, context) => withInfo('')(story)(context));
 stories.addDecorator(withKnobs);
 
 stories.add('with default loader', () => (
-  <Loading />
+    <Loading />
 ));
 
 stories.add('with big size', () => (
-  <Loading size='large' />
+    <Loading size="large" />
 ));
 
 stories.add('with medium size and context "good"', () => (
-  <Loading size='medium' context='good' />
+    <Loading size="medium" context="good" />
 ));
 
 stories.add('with dynamic properties', () => {
-  const context = select('Context', props.contexts, props.contexts[0]);
-  const size = select('Size', props.sizes, props.sizes[0]);
-  
-  return <Loading size={size} context={context} />
+    const context = select('Context', props.contexts, props.contexts[0]);
+    const size = select('Size', props.sizes, props.sizes[0]);
+    const hidden = boolean('Hidden', false);
+
+    return (<Loading
+        size={ size }
+        context={ context }
+        hidden={ hidden }
+    />);
 });
