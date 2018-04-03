1ca214dbdb614c985921f9732a5a5c2e438239cd
Carlo Bernardini <bernardini@textkernel.nl>
2018-04-03 17:17:08 +0200

    Added snapshot

---
+++ tests/Toggle/Toggle.spec.js
@@ -0,0 +1,24 @@
+import React from 'react';
+import Toggle from '../../lib/Toggle';
+
+describe('<Toggle />', () => {
+    it('should render a simple Toggle', () => {
+        const wrapper = shallow(<Toggle>Label</Toggle>);
+        expect(wrapper).toMatchSnapshot();
+    });
+
+    it('should render a disabled Toggle', () => {
+        const wrapper = shallow(<Toggle disabled>Disabled toggle</Toggle>);
+        expect(wrapper).toMatchSnapshot();
+    });
+
+    it('should render a small primary Toggle that is on by default', () => {
+        const wrapper = shallow(<Toggle context="primary" small on>Label</Toggle>);
+        expect(wrapper).toMatchSnapshot();
+    });
+
+    it('should render a toggle with custom class + muted label when switched off', () => {
+        const wrapper = shallow(<Toggle className="custom-class" offMuted>Muted label</Toggle>);
+        expect(wrapper).toMatchSnapshot();
+  });
+});
