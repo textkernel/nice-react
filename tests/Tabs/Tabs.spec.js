ef9f293a664af8b68ad6a5c9fda33385cb241f56
Mauricio Minella <minella@textkernel.nl>
2018-05-03 10:08:56 +0200

    Basic tests for Tabs component created. I'm currently skipping the Tab.spec.js tests as they need to be fixed

--- tests/Tabs/Tabs.spec.js
+++ tests/Tabs/Tabs.spec.js
@@ -1,55 +1,65 @@
 import React from 'react';
 import { Tabs, Tab } from '../../lib/Tabs';
-import { Button, ButtonGroup } from '../../lib/Button';
 
-describe.skip('<Tabs/>', () => {
+describe('<Tabs/>', () => {
 
-    let Component = null;
-
-    beforeEach(() => {
-        Component = shallow(
+    it('should render', () => {
+        const Component = shallow(
             <Tabs>
-                <Tab iconClassName={'icon-class-0'}
-                    linkClassName={'link-class-0'}>
-                    <p>content 0</p>
+                <Tab headerText="First" badge="56" icon="fa-home">
+                    <p>Tab number zero</p>
                 </Tab>
-                <Tab iconClassName={'icon-class-1'}
-                    linkClassName={'link-class-1'}>
-                    <p>content 1</p>
+                <Tab headerText="Second" icon="fa-angle-down">
+                    <p>Tab number one</p>
                 </Tab>
             </Tabs>
         );
-    });
-
-    it('should render', () => {
-        expect(Component.length).toBeTruthy();
+        expect(Component).toMatchSnapshot();
     });
 
     it('should render tab 0 content by default', () => {
-        expect(Component.find('.tabs-active-content').text()).toEqual('content 0');
+        const Component = shallow(
+            <Tabs>
+                <Tab headerText="First" badge="56" icon="fa-home">
+                    <p>Tab number zero</p>
+                </Tab>
+                <Tab headerText="Second" icon="fa-angle-down">
+                    <p>Tab number one</p>
+                </Tab>
+            </Tabs>
+        );
+        expect(Component.find('.nice-tab-content').text()).toEqual('Tab number zero');
     });
 
     it("should allow to specify the 'defaultActiveTabIndex'", () => {
         const Component = shallow(
             <Tabs defaultActiveTabIndex={1}>
-                <Tab iconClassName={'icon-class-0'}
-                    linkClassName={'link-class-0'}>
-                    <p>content 0</p>
+                <Tab headerText="First" badge="56" icon="fa-home">
+                    <p>Tab number zero</p>
                 </Tab>
-                <Tab iconClassName={'icon-class-1'}
-                    linkClassName={'link-class-1'}>
-                    <p>content 1</p>
+                <Tab headerText="Second" icon="fa-angle-down">
+                    <p>Tab number one</p>
                 </Tab>
             </Tabs>
         );
         // Active tab content should be 1 instead of 0 now
-        expect(Component.find('.tabs-active-content').text()).toEqual('content 1');
+        expect(Component.find('.nice-tab-content').text()).toEqual('Tab number one');
     });
 
     it('should change tab content to the selected tab', () => {
-        expect(Component.find('.tabs-active-content').text()).toEqual('content 0');
+        const Component = mount(
+            <Tabs defaultActiveTabIndex={0}>
+                <Tab headerText="First" badge="56" icon="fa-home">
+                    <p>Tab number zero</p>
+                </Tab>
+                <Tab headerText="Second" icon="fa-angle-down">
+                    <p>Tab number one</p>
+                </Tab>
+            </Tabs>
+        );
+        expect(Component.find('.nice-tab-content').text()).toEqual('Tab number zero');
         // Select tab at index 1, and expect text to change
-        Component.find('.link-class-1').simulate('click', { preventDefault: () => { } });
-        expect(Component.find('.tabs-active-content').text()).toEqual('content 1');
+        Component.find('.tab-link-class-1').simulate('click', { preventDefault: () => { } });
+        expect(Component.find('.nice-tab-content').text()).toEqual('Tab number one');
     });
 });
