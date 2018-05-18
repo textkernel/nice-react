16e4739bda04f23707d209dc5d63c160f2791dc1
Carlo Bernardini <bernardini@textkernel.nl>
2018-05-18 16:02:52 +0200

    Fixed eslint errors and failing tests on toggle component

--- tests/Toggle/Toggle.spec.js
+++ tests/Toggle/Toggle.spec.js
@@ -3,27 +3,27 @@ import Toggle from '../../lib/Toggle';
 
 describe('<Toggle />', () => {
     it('should render a simple Toggle', () => {
-        const wrapper = shallow(<Toggle>Label</Toggle>);
+        const wrapper = shallow(<Toggle id="myToggle">Label</Toggle>);
         expect(wrapper).toMatchSnapshot();
     });
 
     it('should render a disabled Toggle', () => {
-        const wrapper = shallow(<Toggle disabled>Disabled toggle</Toggle>);
+        const wrapper = shallow(<Toggle id="myToggle" disabled>Disabled toggle</Toggle>);
         expect(wrapper).toMatchSnapshot();
     });
 
     it('should render a small primary Toggle that is on by default', () => {
-        const wrapper = shallow(<Toggle context="primary" small defaultOn>Label</Toggle>);
+        const wrapper = shallow(<Toggle id="myToggle" context="primary" small defaultOn>Label</Toggle>);
         expect(wrapper).toMatchSnapshot();
     });
 
     it('should render a controlled Toggle that is off by state', () => {
-        const wrapper = shallow(<Toggle context="primary" small on={ false }>Label</Toggle>);
+        const wrapper = shallow(<Toggle id="myToggle" context="primary" small on={ false }>Label</Toggle>);
         expect(wrapper).toMatchSnapshot();
     });
 
     it('should render a toggle with custom class / style + muted label when switched off', () => {
-        const wrapper = shallow(<Toggle className="custom-class" style={{ visibility: 'hidden' }} offMuted>Muted label</Toggle>);
+        const wrapper = shallow(<Toggle id="myToggle" className="custom-class" style={{ visibility: 'hidden' }} offMuted>Muted label</Toggle>);
         expect(wrapper).toMatchSnapshot();
   });
 });
