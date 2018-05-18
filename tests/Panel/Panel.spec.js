cbc0804f9b2cab361995d39d8324feb9e33e13f4
Carlo Bernardini <bernardini@textkernel.nl>
2018-05-18 17:19:28 +0200

    Add support for custom classnames and inline styles for Panel and fixed / moved tests

--- tests/Panel/Panel.spec.js
+++ tests/Panel/Panel.spec.js
@@ -4,21 +4,26 @@ import Panel from '../../lib/Panel';
 describe('<Panel />', () => {
   it('should render content  the Panel', () => {
     const wrapper = shallow(<Panel>Hello Jest!</Panel>);
-    expect(wrapper.find('.nice-panel-content').text()).toEqual('Hello Jest!');
+    expect(wrapper).toMatchSnapshot();
   });
 
   it('should render Panel with title', () => {
     const wrapper = shallow(<Panel title='My title' />);
-    expect(wrapper.find('.nice-panel-heading').text()).toEqual('My title');
+    expect(wrapper).toMatchSnapshot();
   });
 
   it('should render the Panel header on the bottom', () => {
     const wrapper = shallow(<Panel title='My title' inverseHeading />);
-    expect(wrapper.find('div').at(2).hasClass('nice-panel-heading')).toBe(true);
+    expect(wrapper).toMatchSnapshot();
   });
 
   it('should render with different color theme', () => {
     const wrapper = shallow(<Panel context='good' />);
-    expect(wrapper.find('.nice-panel').hasClass('good')).toBe(true);
+    expect(wrapper).toMatchSnapshot();
+  });
+
+  it('should render a Panel with custom classes / styles', () => {
+    const wrapper = shallow(<Panel className="test" style={{ display: 'none' }}>Hello Jest!</Panel>);
+    expect(wrapper).toMatchSnapshot();
   });
 });
