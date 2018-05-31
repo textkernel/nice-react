3fc327300f0f28bb3ca268c4c322c736d44bddfb
Guilherme <ribeiro@textkernel.nl>
2018-05-31 11:18:46 +0200

    Added style and classname to Breadcrumb

--- tests/Breadcrumb/Breadcrumb.spec.js
+++ tests/Breadcrumb/Breadcrumb.spec.js
@@ -4,23 +4,28 @@ import Breadcrumb from '../../lib/Breadcrumb';
 describe('<Breadcrumb />', () => {
     it('should render default Breadcrumb', () => {
         const wrapper = shallow(
-          <Breadcrumb>
-            <Breadcrumb.Item text="Level 1" />
-            <Breadcrumb.Item text="Level 2" />
-            <Breadcrumb.Item text="Level 3" />
-          </Breadcrumb>
+            <Breadcrumb>
+                <Breadcrumb.Item text="Level 1" />
+                <Breadcrumb.Item text="Level 2" />
+                <Breadcrumb.Item text="Level 3" />
+            </Breadcrumb>
         );
         expect(wrapper).toMatchSnapshot();
     });
 
     it('should render small Breadcrumb', () => {
         const wrapper = shallow(
-          <Breadcrumb size="small">
-            <Breadcrumb.Item text="Level 1" />
-            <Breadcrumb.Item text="Level 2" />
-            <Breadcrumb.Item text="Level 3" />
-          </Breadcrumb>
+            <Breadcrumb size="small">
+                <Breadcrumb.Item text="Level 1" />
+                <Breadcrumb.Item text="Level 2" />
+                <Breadcrumb.Item text="Level 3" />
+            </Breadcrumb>
         );
+        expect(wrapper).toMatchSnapshot();
+    });
+
+    it('should render Breadcrumb with className and style', () => {
+        const wrapper = shallow(<Breadcrumb className="my-classname" style={{ top: 5 }} />);
         expect(wrapper).toMatchSnapshot();
     });
 });
