c5ab794e30029cf8b22ac1e9a28177ccfcb37090
Alex Antipin <antipin@textkernel.nl>
2018-06-28 19:12:58 +0200

    TMP COMMIT

--- config/webpack.config.dev.js
+++ config/webpack.config.dev.js
@@ -36,10 +36,18 @@ module.exports = {
                 use: ['babel-loader'],
                 exclude: /node_modules/,
             },
+            // {
+            //     test: /\.css$/,
+            //     use: ['style-loader', 'css-loader']
+            //
+            // },
             {
-                test: /\.css$/,
-                use: ['style-loader', 'css-loader']
-                    
+                test: /\.scss$/,
+                use: [
+                    "style-loader", // creates style nodes from JS strings
+                    "css-loader", // translates CSS into CommonJS
+                    "sass-loader" // compiles Sass to CSS
+                ]
             },
             {
                 test: /\.(png|jpg)$/,
--- storybook/webpack.config.js
+++ storybook/webpack.config.js
@@ -1,6 +1,9 @@
 const path = require('path');
 const genDefaultConfig = require('@storybook/react/dist/server/config/defaults/webpack.config.js');
 
+console.log('\n\n------------');
+console.log('config', genDefaultConfig);
+
 /**
  * Extend the current storybook webpack config to resolve the library components path
  */
